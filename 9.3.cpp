ea5accbd0ca011a1d6fe752f77746b2877645f9c
georgyteterin <teterin.normal@gmail,com>
2022-05-20 01:25:32 +0300

    made transpose method

---
+++ 9.3.cpp
@@ -0,0 +1,73 @@
+﻿#include <iostream>
+#include <string>
+#include <clocale>
+using namespace std;
+
+class Matrix {
+private:
+	int rows, columns;
+	int** arr;
+public:
+
+	Matrix() {
+		rows = 0;
+		columns = 0;
+		arr = new int* [rows];
+	}
+
+	~Matrix() {
+		cout << "вызван деструктор\n";
+	}
+
+	void setMatrix() {
+		//cout << "setMatrix is on proccess\n";
+		cout << "enter rows count: ";
+		cin >> rows;
+		cout << "enter columns count: ";
+		cin >> columns;
+
+		for (int i = 0; i < rows; i++) {
+			arr[i] = new int[columns];
+		}
+
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < columns; j++) {
+				arr[i][j] = rand() % 20;
+			}
+		}
+	}
+
+	void getMatrix() {
+		//cout << "getMatrix is on proccess\n";
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < columns; j++) {
+				cout << arr[i][j] << "\t";
+			}
+			cout << endl;
+		}
+		cout << endl;
+	}
+	
+	void transpose() {
+		for (int i = 0; i < columns; i++) {
+			for (int j = 0; j < rows; j++) {
+				cout << arr[j][i] << "\t";
+			}
+			cout << endl;
+		}
+	}
+
+};
+
+
+int main() {
+	setlocale(LC_CTYPE, "rus");
+	Matrix mat;
+	mat.setMatrix();
+	mat.getMatrix();
+	mat.transpose();
+	Matrix* matt = new Matrix();
+	delete matt;
+
+	return 0;
+}
